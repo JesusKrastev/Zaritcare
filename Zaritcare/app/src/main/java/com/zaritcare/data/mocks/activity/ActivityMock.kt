627211e5fafe9adae9f2cfd573387980d2e354b2
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-19 17:54:27 +0100

    feat: Added Mock and DaoMock of Activity

---
+++ Zaritcare/app/src/main/java/com/zaritcare/data/mocks/activity/ActivityMock.kt
@@ -0,0 +1,11 @@
+package com.zaritcare.data.mocks.activity
+
+data class ActivityMock(
+    val id: Int,
+    val image: String,
+    val name: String,
+    val description: String,
+    val authorQuote: String?,
+    val quote: String?,
+    val action: String
+)
