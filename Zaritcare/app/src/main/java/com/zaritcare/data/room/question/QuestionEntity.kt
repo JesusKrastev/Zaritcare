58835e1c61749a935eff3295b800527b65e46a56
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-19 18:46:27 +0100

    feat: Deleted the field call answer in QuestionEntity

--- Zaritcare/app/src/main/java/com/zaritcare/data/room/question/QuestionEntity.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/data/room/question/QuestionEntity.kt
@@ -11,8 +11,6 @@ data class QuestionEntity(
     val id: Int,
     @ColumnInfo(name = "question")
     val question: String,
-    @ColumnInfo(name = "answer")
-    val answer: String,
     @ColumnInfo(name = "category")
     val category: String
 )
