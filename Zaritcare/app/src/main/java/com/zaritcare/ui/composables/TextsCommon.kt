06d83090ffd3bf50eb3c2f769aee1e7f773d3e8c
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-19 20:00:02 +0100

    feat: Added new composable of Texts

--- Zaritcare/app/src/main/java/com/zaritcare/ui/composables/TextsCommon.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/ui/composables/TextsCommon.kt
@@ -1,2 +1,80 @@
 package com.zaritcare.ui.composables
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun TextTile(
+    title: String,
+    color: Color = MaterialTheme.colorScheme.onBackground,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = title,
+        color = color,
+        fontWeight = FontWeight.Bold,
+        style = MaterialTheme.typography.titleLarge,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TextBody(
+    text: String,
+    color: Color = MaterialTheme.colorScheme.onBackground,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyLarge,
+        color = color,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TextBody(
+    text: AnnotatedString,
+    color: Color = MaterialTheme.colorScheme.onBackground,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyLarge,
+        color = color,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TextLabel(
+    text: String,
+    color: Color = MaterialTheme.colorScheme.onBackground,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        color = color,
+        style = MaterialTheme.typography.labelLarge,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun DefaultText(
+    text: String,
+    color: Color = MaterialTheme.colorScheme.onBackground,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        color = color,
+        modifier = modifier
+    )
+}
