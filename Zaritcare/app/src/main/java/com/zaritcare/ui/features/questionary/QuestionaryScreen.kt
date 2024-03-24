fb7fdf3ae7692b9a4693a93793f4d7d8ac561d52
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-24 20:11:23 +0100

    feat: Added CollapsingLayout in QuestionaryScreen

--- Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/QuestionaryScreen.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/QuestionaryScreen.kt
@@ -5,21 +5,29 @@ import com.zaritcare.ui.composables.TextSwitch
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.FloatingActionButtonElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.zaritcare.ui.composables.CollapsingLayout
 import com.zaritcare.ui.features.components.ZaritcareNavBar
 import com.zaritcare.ui.features.questionary.wellbeingform.EmotionUiState
 import com.zaritcare.ui.features.questionary.wellbeingform.WellbeingScaleScreen
@@ -50,17 +58,43 @@ fun SaveButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
-    Button(
+    FloatingActionButton(
         modifier = modifier,
         onClick = onClick,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.onPrimary
-        ),
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        elevation = FloatingActionButtonDefaults.elevation(0.dp)
     ) {
         Icon(
             imageVector = Icons.Filled.Save,
             contentDescription = "Save"
+        )
+    }
+}
+
+@Composable
+fun Form(
+    modifier: Modifier = Modifier,
+    selectedIndex: Int,
+    questions: List<QuestionUiState>,
+    onChangeAnswer: (QuestionUiState) -> Unit,
+    emotions: List<EmotionUiState>,
+    onClickSave: () -> Unit
+) {
+    Column(
+        modifier = modifier.verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        ListQuestions(
+            selectedIndex = selectedIndex,
+            questions = questions,
+            onChangeAnswer = onChangeAnswer,
+            emotions = emotions
+        )
+        SaveButton(
+            modifier = Modifier.padding(top = 16.dp),
+            onClick = onClickSave
         )
     }
 }
@@ -76,27 +110,30 @@ fun Content(
     onSelectionChange: (Int) -> Unit,
     categories: List<CategoryUiState>,
 ) {
-    Column(
-        modifier = modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        TextSwitch(
-            selectedIndex = selectedIndex,
-            items = categories.map { it.name },
-            onSelectionChange = onSelectionChange
-        )
-        ListQuestions(
-            selectedIndex = selectedIndex,
-            questions = questions,
-            onChangeAnswer = onChangeAnswer,
-            emotions = emotions
-        )
-        SaveButton(
-            modifier = Modifier.padding(top = 16.dp),
-            onClick = onClickSave
-        )
-    }
+    CollapsingLayout(
+        modifier = modifier.fillMaxWidth(),
+        collapsingTop = { },
+        bodyContent = {
+            Column(
+                modifier = modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                TextSwitch(
+                    selectedIndex = selectedIndex,
+                    items = categories.map { it.name },
+                    onSelectionChange = onSelectionChange
+                )
+                Form(
+                    selectedIndex = selectedIndex,
+                    questions = questions,
+                    onChangeAnswer = onChangeAnswer,
+                    emotions = emotions,
+                    onClickSave = onClickSave
+                )
+            }
+        }
+    )
 }
 
 @Composable
