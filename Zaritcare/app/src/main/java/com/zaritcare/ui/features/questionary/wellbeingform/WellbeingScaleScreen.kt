18db3f03438bae24dbccefe8090da45cfe5a68dc
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-24 19:33:21 +0100

    feat: Added scroll in WellbeingScaleScreen

--- Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/wellbeingform/WellbeingScaleScreen.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/wellbeingform/WellbeingScaleScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -146,7 +148,9 @@ fun WellbeingScaleScreen(
     emotions: List<EmotionUiState>
 ) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         Form(
             onChangeAnswer = onChangeAnswer,
