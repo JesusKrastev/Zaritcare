276c0403dab491a42fb60cda47fd2529c91c6274
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-24 19:20:59 +0100

    fix: Fixed a error when put data in the database

--- Zaritcare/app/src/main/java/com/zaritcare/ZaritcareApp.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/ZaritcareApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.util.Log
 import com.zaritcare.data.QuestionRepository
 import com.zaritcare.data.mocks.activity.ActivityDaoMock
+import com.zaritcare.data.mocks.category.CategoryDaoMock
 import com.zaritcare.data.mocks.emotion.EmotionDaoMock
 import com.zaritcare.data.mocks.question.QuestionDaoMock
 import com.zaritcare.data.room.activity.ActivityDao
@@ -13,6 +14,8 @@ import com.zaritcare.data.room.question.QuestionDao
 import com.zaritcare.data.room.question.QuestionEntity
 import com.zaritcare.data.toActivity
 import com.zaritcare.data.toActivityEntity
+import com.zaritcare.data.toCategory
+import com.zaritcare.data.toCategoryEntity
 import com.zaritcare.data.toEmotion
 import com.zaritcare.data.toEmotionEntity
 import com.zaritcare.data.toQuestion
@@ -36,26 +39,31 @@ class ZaritcareApp: Application() {
     @Inject
     lateinit var questionDaoEntity: QuestionDao
     @Inject
-    lateinit var categoryDao: CategoryDao
+    lateinit var categoryDaoMock: CategoryDaoMock
     @Inject
-    lateinit var questionRepository: QuestionRepository
+    lateinit var categoryDaoEntity: CategoryDao
 
     override fun onCreate() {
         super.onCreate()
         runBlocking {
-            if (activityDaoMock.count() == 0){
+            if(categoryDaoEntity.count() == 0){
+                categoryDaoMock.get().forEach { category ->
+                    categoryDaoEntity.insert(category.toCategory().toCategoryEntity())
+                }
+            }
+            if (activityDaoEntity.count() == 0){
                 activityDaoMock.get().forEach { activity ->
                     activityDaoEntity.insert(activity.toActivity().toActivityEntity())
                 }
             }
-            if (emotionDaoMock.count() == 0){
+            if (emotionDaoEntity.count() == 0){
                 emotionDaoMock.get().forEach { emotion ->
                     emotionDaoEntity.insert(emotion.toEmotion().toEmotionEntity())
                 }
             }
-            if (questionDaoMock.count() == 0){
+            if (questionDaoEntity.count() == 0){
                 questionDaoMock.get().forEach { question ->
-                    questionDaoEntity.insert(question.toQuestion(categoryDao).toQuestionEntity(categoryDao))
+                    questionDaoEntity.insert(question.toQuestion(categoryDaoEntity).toQuestionEntity(categoryDaoEntity))
                 }
             }
         }
