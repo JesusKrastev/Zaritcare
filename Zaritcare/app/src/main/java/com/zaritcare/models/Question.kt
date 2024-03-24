254e49a0f2338e579f2a43d852c303e7f8efdbc4
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-24 19:45:26 +0100

    feat: Added new fields indicators in Question

--- Zaritcare/app/src/main/java/com/zaritcare/models/Question.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/models/Question.kt
@@ -4,6 +4,8 @@ data class Question(
     val id: Int,
     val question: String,
     val category: String,
+    val minimumValueIndicator: String,
+    val maximumValueIndicator: String,
     val type: QuestionType
 ) {
     enum class QuestionType {
