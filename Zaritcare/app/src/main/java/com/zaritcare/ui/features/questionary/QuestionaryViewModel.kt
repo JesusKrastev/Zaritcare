0a7037a06ac5069445b237ea4b1492ed3eeab63e
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-24 19:42:30 +0100

    feat: Modified QuestionaryViewModel

--- Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/QuestionaryViewModel.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/QuestionaryViewModel.kt
@@ -37,12 +37,6 @@ class QuestionaryViewModel @Inject constructor(
             try {
                 val emotions: List<EmotionUiState> = getEmotions()
                 questionsState = getQuestions().map { it.copy(answer = if(it.type == QuestionUiState.QuestionUiType.EMOTION) emotions.first().name else "0") }
-//                getQuestions().collect { questions ->
-//                    questionsState = questions.map { question ->
-//                        val questionState = question.toQuestionUiState()
-//                        questionState.copy(answer = if(questionState.type == QuestionUiState.QuestionUiType.EMOTION) emotions.first().name else "0")
-//                    }
-//                }
             } catch (e: Exception) {
                 Log.d("QuestionaryViewModel", "Error loading questions", e)
             }
