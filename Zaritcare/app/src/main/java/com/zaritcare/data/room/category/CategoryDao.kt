04c1ae07a3fcf1ab1acb7eff0f6302d094599519
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-24 19:22:10 +0100

    feat: Added new methods to CategoryDao

--- Zaritcare/app/src/main/java/com/zaritcare/data/room/category/CategoryDao.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/data/room/category/CategoryDao.kt
@@ -1,7 +1,9 @@
 package com.zaritcare.data.room.category
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import com.zaritcare.data.room.question.QuestionEntity
 
 @Dao
 interface CategoryDao {
@@ -9,6 +11,10 @@ interface CategoryDao {
     suspend fun get(): List<CategoryEntity>
     @Query("SELECT * FROM categories WHERE name = :name")
     suspend fun get(name: String): CategoryEntity
+    @Insert
+    suspend fun insert(categoryEntity: CategoryEntity)
     @Query("SELECT * FROM categories WHERE id = :id")
     suspend fun get(id: Int): CategoryEntity
+    @Query("SELECT COUNT(*) FROM categories")
+    suspend fun count(): Int
 }
