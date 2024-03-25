b71558c293e655cf8368a7ac859849efed3aa19d
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-25 16:21:35 +0100

    style: Modified the imports of the ZaritScaleScreen

--- Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/zaritform/ZaritScaleScreen.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/zaritform/ZaritScaleScreen.kt
@@ -3,13 +3,11 @@ package com.zaritcare.ui.features.questionary.zaritform
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.zaritcare.ui.features.questionary.QuestionUiState
-import com.zaritcare.ui.features.questionary.selectrangequestion.SelectRangeQuestion
+import com.zaritcare.ui.features.questionary.components.SelectRangeQuestion
 
 @Composable
 fun Form(
