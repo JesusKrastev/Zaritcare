3bd32884f52214ee60ea151e0922c672a1047a73
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-25 11:50:12 +0100

    feat: Added new fields

--- Zaritcare/app/src/main/java/com/zaritcare/data/mocks/activity/ActivityMock.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/data/mocks/activity/ActivityMock.kt
@@ -2,8 +2,9 @@ package com.zaritcare.data.mocks.activity
 
 data class ActivityMock(
     val id: Int,
+    val frontPage: String,
     val image: String,
-    val name: String,
+    val title: String,
     val description: String,
     val authorQuote: String?,
     val quote: String?,
