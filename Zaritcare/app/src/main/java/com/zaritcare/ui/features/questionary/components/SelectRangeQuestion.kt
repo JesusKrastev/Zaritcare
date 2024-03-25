e88721abcad140588b4a6f6b5c7e3a7825d0c9c2
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-25 16:18:04 +0100

    feat: Modified the folder call SelectRangeQuestion to Components

--- Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/components/SelectRangeQuestion.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/ui/features/questionary/components/SelectRangeQuestion.kt
@@ -1,4 +1,4 @@
-package com.zaritcare.ui.features.questionary.selectrangequestion
+package com.zaritcare.ui.features.questionary.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
