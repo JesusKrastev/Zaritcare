7357a3cc00ca971f0b934b0a4f083b72b4a9380e
Jesus Krastev <jesuskrastev@gmail.com>
2024-03-26 21:59:39 +0100

    refactor: Refactor all the code of CircularProgressIndicator

--- Zaritcare/app/src/main/java/com/zaritcare/ui/composables/CircularProgressIndicator.kt
+++ Zaritcare/app/src/main/java/com/zaritcare/ui/composables/CircularProgressIndicator.kt
@@ -1,163 +1,158 @@
 package com.zaritcare.ui.composables
 
-import android.graphics.Paint
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.zaritcare.ui.theme.ZaritcareTheme
-import kotlin.math.PI
-import kotlin.math.atan2
-import kotlin.math.roundToInt
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
-fun CircularProgressIndicator(
-    modifier: Modifier = Modifier,
-    initialValue:Int = 0,
-    primaryColor: Color = MaterialTheme.colorScheme.primary,
-    secondaryColor:Color = MaterialTheme.colorScheme.secondary,
-    textColor: Color = MaterialTheme.colorScheme.onBackground,
-    text: String,
-    minValue:Int = 0,
-    enableDragging: Boolean = true,
-    maxValue:Int = 60,
-    circleRadius:Float,
-    onPositionChange:(Int)->Unit
+private fun ProgressCircle(
+    currentProgress: Int,
+    maxProgress: Int,
+    radius: Dp,
+    activeBarColor: Color,
+    handleColor: Color,
+    inactiveBarColor: Color,
+    strokeWidth: Dp
 ) {
-    var circleCenter: Offset by remember { mutableStateOf(Offset.Zero) }
-    var positionValue: Int by remember { mutableIntStateOf(initialValue) }
-    var changeAngle: Float by remember { mutableFloatStateOf(0f) }
-    var dragStartedAngle: Float by remember { mutableFloatStateOf(0f) }
-    var oldPositionValue: Int by remember { mutableIntStateOf(initialValue) }
+    Canvas(
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        val progressAngle: Float = 360f * currentProgress / maxProgress
+        val pointX: Float = size.width / 2f + (size.width / 2f) * cos(Math.toRadians(90 + progressAngle.toDouble())).toFloat()
+        val pointY: Float = size.height / 2f + (size.height / 2f) * sin(Math.toRadians(90 + progressAngle.toDouble())).toFloat()
+        drawArc(
+            color = inactiveBarColor,
+            startAngle = 90f,
+            sweepAngle = 360f,
+            useCenter = false,
+            style = Stroke(width = strokeWidth.toPx())
+        )
+        drawArc(
+            color = activeBarColor,
+            startAngle = 90f,
+            sweepAngle = 360f * currentProgress / maxProgress,
+            useCenter = false,
+            style = Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
+        )
+        drawCircle(
+            color = handleColor,
+            radius = (radius * 0.15f).value.dp.toPx(),
+            center = Offset(pointX, pointY)
+        )
+    }
+}
 
+@Composable
+private fun EditableProgressField(
+    currentProgress: Int,
+    maxProgress: Int,
+    radius: Dp,
+    textColor: Color,
+    isEditable: Boolean = true,
+    indicatorText: String,
+    onChangeValue: (Int) -> Unit = {}
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        BasicTextField(
+            value = currentProgress.toString(),
+            onValueChange = {
+                if(it.isEmpty()) {
+                    onChangeValue(0)
+                } else if(currentProgress == 0) {
+                    onChangeValue(it.replace("0", "").toInt())
+                } else if(it.length <= maxProgress.toString().length && it.toInt() <= maxProgress) {
+                    onChangeValue(it.toInt())
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            textStyle = TextStyle(
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontSize = (radius * 0.5f).value.sp
+            ),
+            enabled = isEditable,
+            singleLine = true,
+            maxLines = 1
+        )
+        Text(
+            text = indicatorText,
+            color = textColor,
+            fontSize = (radius * 0.3f).value.sp
+        )
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    currentProgress: Int,
+    maxProgress: Int = 60,
+    indicatorText: String,
+    radius: Dp = 50.dp,
+    isEditable: Boolean = true,
+    activeBarColor: Color = MaterialTheme.colorScheme.primary,
+    handleColor: Color = MaterialTheme.colorScheme.onPrimary,
+    inactiveBarColor: Color = MaterialTheme.colorScheme.secondary,
+    textColor: Color = MaterialTheme.colorScheme.onBackground,
+    strokeWidth: Dp = (radius * 0.15f).value.dp,
+    onChangeValue: (Int) -> Unit = {}
+) {
     Box(
-        modifier = modifier.wrapContentSize()
-    ){
-        Canvas(
-            modifier = Modifier
-                .size((circleRadius).dp)
-                .pointerInput(true){
-                    detectDragGestures(
-                        onDragStart = {offset ->
-                            if(enableDragging) {
-                                dragStartedAngle = -atan2(
-                                    x = circleCenter.y - offset.y,
-                                    y = circleCenter.x - offset.x
-                                ) * (180f / PI).toFloat()
-                                dragStartedAngle = (dragStartedAngle + 180f).mod(360f)
-                            }
-                        },
-                        onDrag = { change, _ ->
-                            var touchAngle: Float = -atan2(
-                                x = circleCenter.y - change.position.y,
-                                y = circleCenter.x - change.position.x
-                            ) * (180f / PI).toFloat()
-                            touchAngle = (touchAngle + 180f).mod(360f)
-
-                            val currentAngle: Float = oldPositionValue*360f/(maxValue-minValue)
-                            changeAngle = touchAngle - currentAngle
-
-                            val lowerThreshold: Float = currentAngle - (360f / (maxValue-minValue) * 5)
-                            val higherThreshold: Float = currentAngle + (360f / (maxValue-minValue) * 5)
-
-                            if(dragStartedAngle in lowerThreshold .. higherThreshold) {
-                                positionValue = (oldPositionValue + (changeAngle / (360f / (maxValue - minValue))).roundToInt())
-                            }
-                        },
-                        onDragEnd = {
-                            oldPositionValue = positionValue
-                            onPositionChange(positionValue)
-                        }
-                    )
-                }
-        ){
-            val width: Float = size.width
-            val height: Float = size.height
-            val circleThickness: Float = width / 20f
-            circleCenter = Offset(x = width / 2f, y = height / 2f)
-
-            drawCircle(
-                style = Stroke(
-                    width = circleThickness
-                ),
-                color = secondaryColor,
-                radius = circleRadius,
-                center = circleCenter
-            )
-            drawArc(
-                color = primaryColor,
-                startAngle = 90f,
-                sweepAngle = (360f/maxValue) * positionValue.toFloat(),
-                style = Stroke(
-                    width = circleThickness,
-                    cap = StrokeCap.Round
-                ),
-                useCenter = false,
-                size = Size(
-                    width = circleRadius * 2f,
-                    height = circleRadius * 2f
-                ),
-                topLeft = Offset(
-                    (width - circleRadius * 2f) / 2f,
-                    (height - circleRadius * 2f) / 2f
-                )
-
-            )
-            drawContext.canvas.nativeCanvas.apply {
-                drawIntoCanvas {
-                    drawText(
-                        "$positionValue".padStart(2, '0'),
-                        circleCenter.x,
-                        circleCenter.y,
-                        Paint().apply {
-                            textSize = circleRadius * 0.6f
-                            textAlign = Paint.Align.CENTER
-                            color = textColor.toArgb()
-                            isFakeBoldText = true
-                        }
-                    )
-                    drawText(
-                        text,
-                        circleCenter.x,
-                        circleCenter.y + circleRadius * 0.3f,
-                        Paint().apply {
-                            textSize = circleRadius * 0.3f
-                            textAlign = Paint.Align.CENTER
-                            color = textColor.toArgb()
-                            isFakeBoldText = false
-                        }
-                    )
-                }
-            }
-        }
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        ProgressCircle(
+            currentProgress = currentProgress,
+            maxProgress = maxProgress,
+            radius = radius,
+            activeBarColor = activeBarColor,
+            handleColor = handleColor,
+            inactiveBarColor = inactiveBarColor,
+            strokeWidth = strokeWidth
+        )
+        EditableProgressField(
+            currentProgress = currentProgress,
+            maxProgress = maxProgress,
+            radius = radius,
+            textColor = textColor,
+            isEditable = isEditable,
+            indicatorText = indicatorText,
+            onChangeValue = onChangeValue
+        )
     }
 }
 
@@ -167,6 +162,8 @@ fun TimerPreview() {
     ZaritcareTheme(
         darkTheme = true
     ) {
+        var progress: Int by remember { mutableIntStateOf(10) }
+
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
@@ -176,16 +173,15 @@ fun TimerPreview() {
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center
             ) {
-                CircularProgressIndicator(
-                    text = "minutos",
-                    circleRadius = 150f,
-                    onPositionChange = {
-                    }
-                )
-                CircularProgressIndicator(
-                    text = "minutos",
-                    circleRadius = 150f,
-                    onPositionChange = {
+                CircularProgressBar(
+                    currentProgress = progress,
+                    radius = 50.dp,
+                    activeBarColor = MaterialTheme.colorScheme.primary,
+                    inactiveBarColor = MaterialTheme.colorScheme.secondary,
+                    textColor = MaterialTheme.colorScheme.onBackground,
+                    indicatorText = "minutos",
+                    onChangeValue = {
+                        progress = it
                     }
                 )
             }
